96ee38f008f414543ea951088792dce4ff6a3dae
Henry Hale <92443116+henryhale@users.noreply.github.com>
2024-10-25 00:43:34 +0300

    fix: ci build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/pixsim/"
+  base: '/pixsim/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        font: resolve(__dirname, 'packages/font/index.html'),
+        img: resolve(__dirname, 'packages/img/index.html'),
+      },
+    },
+  },
 })
+
